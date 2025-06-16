611050b0161546aca936cf8160fb10aad1c8b5d6
ChieChie <104299226+ChieChieBun@users.noreply.github.com>
2025-06-16 16:32:28 -0400

    students api

--- spec/requests/api/v1/students_controller_spec.rb
+++ spec/requests/api/v1/students_controller_spec.rb
@@ -1,13 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Students", type: :request, skip: true do
+RSpec.describe "Api::V1::Students", type: :request do
   describe "POST /api/v1/students" do
+before do
+      # create existing student to test duplicate email validation
+      Student.create!(
+        first_name: 'John',
+        last_name: 'Turner',
+        email: 'copy@example.com'
+      )
+    end
+
     let(:valid_attributes) do
       {
         student: {
           first_name: Faker::Name.first_name,
           last_name: Faker::Name.last_name,
           email: 'validstudent@example.com'
+        }
+      }
+    end
+      let(:nonvalid_attributes) do
+      {
+        student: {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          email: ''
+        }
+      }
+    end
+    let(:copy_attributes) do
+      {
+        student: {
+          first_name: 'John',
+          last_name: 'Turner',
+          email: 'copy@example.com'
         }
       }
     end
@@ -19,6 +46,22 @@ RSpec.describe "Api::V1::Students", type: :request, skip: true do
 
       expect(response).to have_http_status(:created)
       expect(JSON.parse(response.body)['student']['email']).to eq("validstudent@example.com")
+    end
+    it "doesn't create a new student" do
+      expect {
+        post '/api/v1/students', params: nonvalid_attributes
+      }.to change(Student, :count).by(0)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "doesn't create a new student" do
+
+      expect {
+        post '/api/v1/students', params: copy_attributes
+      }.to change(Student, :count).by(0)
+
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
